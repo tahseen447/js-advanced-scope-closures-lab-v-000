65794d692acee514f71fdadea24577b2124b1397
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-15 21:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function produceDrivingRange(range) {
   return function(a, b){
     let distance = b - a;
-    if( range - distance )
+    if( range - distance > 0  ) {
+      return true;
+    }
+    else 
+    return false;
   }
 }

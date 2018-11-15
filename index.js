b79a1164bdfa1a51b19df210e49624ebba8a7c1f
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-15 21:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function produceDrivingRange(range) {
   return function(a, b){
-    
+      let distance = parseInt(b) - parseInt(a);
+      let value = range - distance;
+      if( value > 0  ) {
+        return `within range by ${value}`;
+      }
+      else{
+        return `${|value|} blocks out of range`;
+      }
+    }
   }
-}

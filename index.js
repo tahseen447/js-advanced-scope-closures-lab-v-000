12278cb37cac9e0e1abf3d1072e19faac4467304
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-15 21:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(range) {
   return function(a, b){
       let distance = parseInt(b) - parseInt(a);
-      let diff = range - distance;
+      let diff = Math.abs(range - distance);
       if( diff > 0  ) {
         return `within range by ${diff}`;
       }
